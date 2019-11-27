c2050ccefe6ff4892efe398fbb4bbfd2edc81a22
Thang <thangbao2698@gmail.com>
2019-11-27 15:45:32 +0700

    update project group 15

--- src/hci15/Scripts/views/base.js
+++ src/hci15/Scripts/views/base.js
@@ -7,124 +7,13 @@ class Base {
     }
 
     InitEvents() {
-        $(document).on('click', '.item', { 'jsObject': this, 'name': 'Nguyễn Văn Thắng' }, this.active);
-        $(document).on('click', '.child-item', { 'jsObject': this, 'name': 'Nguyễn Văn Thắng' }, this.activeChild);
-        $(document).on('click', '#del', this.show);
-        $(document).on('click', '.item', this.addClassChild);
         $(document).on('click', 'button.excel', this.openDialogRequest.bind(this));
         $(document).on('click', 'button.pdf', this.openDialogRequest.bind(this));
         $(document).on('click', 'button.button-request-k', this.closeDialog.bind(this));
         $(document).on('click', 'button.button-request-c', this.openDialog2.bind(this));
         $(document).on('click', 'button.button-request-c-1', this.closeDialog2.bind(this));
-        $(document).on('click', '.ngay', this.addDay);
-        $(document).on('click', '.thang', this.addMonth);
-        $(document).on('click', '.quy', this.addPrecious);
-        $(document).on('click', '.nam', this.addYear);
-        $(document).on('click', '.sanpham', this.addProduct);
-        $(document).on('click', '.canhan', this.quanlycongviec);
-        $(document).on('click', '.task td button', this.chitiet);
     }
 
-    addDay() {
-        $('.includechitiet').addClass('them');
-        $('.includequanlycongviec').addClass('them');
-        $('.includetrang').addClass('them');
-        $('#themthang').addClass('them');
-        $('#themquy').addClass('them');
-        $('#themnam').addClass('them');
-        $('#themsanpham').addClass('them');
-        $('#themngay').removeClass('them');
-    }
-
-    addMonth() {
-        $('.includechitiet').addClass('them');
-        $('.includequanlycongviec').addClass('them');
-        $('.includetrang').addClass('them');
-        $('#themngay').addClass('them');
-        $('#themquy').addClass('them');
-        $('#themnam').addClass('them');
-        $('#themsanpham').addClass('them');
-        $('#themthang').removeClass('them');
-    }
-
-    addPrecious() {
-        $('.includechitiet').addClass('them');
-        $('.includequanlycongviec').addClass('them');
-        $('.includetrang').addClass('them');
-        $('#themngay').addClass('them');
-        $('#themthang').addClass('them');
-        $('#themnam').addClass('them');
-        $('#themsanpham').addClass('them');
-        $('#themquy').removeClass('them');
-    }
-
-    addYear() {
-        $('.includechitiet').addClass('them');
-        $('.includequanlycongviec').addClass('them');
-        $('.includetrang').addClass('them');
-        $('#themngay').addClass('them');
-        $('#themquy').addClass('them');
-        $('#themthang').addClass('them');
-        $('#themsanpham').addClass('them');
-        $('#themnam').removeClass('them');
-    }
-
-    addProduct() {
-        $('.includechitiet').addClass('them');
-        $('.includequanlycongviec').addClass('them');
-        $('.includetrang').addClass('them');
-        $('#themngay').addClass('them');
-        $('#themquy').addClass('them');
-        $('#themnam').addClass('them');
-        $('#themsanpham').removeClass('them');
-        $('#themthang').addClass('them');
-    }
-    quanlycongviec() {
-        $('.includechitiet').addClass('them');
-        $('.includequanlycongviec').removeClass('them');
-        $('.includetrang').addClass('them');
-        $('#themngay').addClass('them');
-        $('#themquy').addClass('them');
-        $('#themnam').addClass('them');
-        $('#themsanpham').addClass('them');
-        $('#themthang').addClass('them');
-    }
-    chitiet() {
-        $('.includechitiet').removeClass('them');
-        $('.includequanlycongviec').addClass('them');
-        $('.includetrang').addClass('them');
-        $('#themngay').addClass('them');
-        $('#themquy').addClass('them');
-        $('#themnam').addClass('them');
-        $('#themsanpham').addClass('them');
-        $('#themthang').addClass('them');
-    }
-
-    active() {
-        $('.nav-menu .item').removeClass('select');
-        $(this).addClass('select');
-    }
-
-    activeChild() {
-        $('.nav-menu .child-item').removeClass('select-child');
-        $(this).addClass('select-child');
-    }
-
-    show() {
-        setTimeout(function () {
-            if (!$('nav-menu #child').hasClass('child')) {
-                $('.nav-menu #child').removeClass('child');
-                $('.nav-menu .child-item').removeClass('select-child');
-            }
-        }, 100);
-    }
-
-    addClassChild() {
-        if (!$('nav-menu #child').hasClass('child')) {
-            $('.nav-menu #child').addClass('child');
-
-        }
-    }
 
     openDialogRe() {
         var cls = arguments[0];
---
+++ src/hci15/Scripts/views/controller.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+
+});
+class Controller {
+    constructor() {
+        this.InitEvents();
+    }
+
+    InitEvents() {
+        $(document).on('click', '.item', { 'jsObject': this, 'name': 'Nguyễn Văn Thắng' }, this.active);
+        $(document).on('click', '.child-item', { 'jsObject': this, 'name': 'Nguyễn Văn Thắng' }, this.activeChild);
+        $(document).on('click', '#del', this.show);
+        $(document).on('click', '.item', this.addClassChild);
+    }
+
+
+    active() {
+        $('.nav-menu .item').removeClass('select');
+        $(this).addClass('select');
+    }
+
+    activeChild() {
+        $('.nav-menu .child-item').removeClass('select-child');
+        $(this).addClass('select-child');
+    }
+
+    show() {
+        setTimeout(function () {
+            if (!$('nav-menu #child').hasClass('child')) {
+                $('.nav-menu #child').removeClass('child');
+                $('.nav-menu .child-item').removeClass('select-child');
+            }
+        }, 100);
+    }
+
+    addClassChild() {
+        if (!$('nav-menu #child').hasClass('child')) {
+            $('.nav-menu #child').addClass('child');
+
+        }
+    }
+
+}
+var baseJS = new Controller();
