998a77511dd756027cb4ba68382a7b1334b9f347
tranhieutuyetmai@gmail.com <tranhieutuyetmai@gmail.com>
2019-12-11 19:25:55 +0700

    Nhom 13 update

---
+++ src/hci13/precache-manifest.a1d37128b39bb18f09d08cee0c67254f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c0bf31b746ff9079bf2dcc7e164d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb4ca1e56f8f5d7d32ef",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "ade40deba32b60ff7c33",
+    "url": "/static/css/main.c00632cf.chunk.css"
+  },
+  {
+    "revision": "bb4ca1e56f8f5d7d32ef",
+    "url": "/static/js/2.c29238e9.chunk.js"
+  },
+  {
+    "revision": "ade40deba32b60ff7c33",
+    "url": "/static/js/main.149426da.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  },
+  {
+    "revision": "cb43de8cb1c230e3994d5556760c13de",
+    "url": "/static/media/jerry.cb43de8c.jpg"
+  }
+]);
