d8c584e8e19a8c10191862aa444346f3cd3815f0
thuydungnguyen <dung.ntt160740@sis.hust.edu.vn>
2019-12-11 13:29:29 +0700

    update code

--- src/hci01/service-worker.js
+++ src/hci01/service-worker.js
@@ -14,11 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-<<<<<<< HEAD
   "/precache-manifest.dc671eb3c4eeaf5ef020e0db0a1d75d3.js"
-=======
-  "/precache-manifest.2917f31a32269f93e1602552f94831c0.js"
->>>>>>> 32b9d5276a5d13af8f7375eea4369d5874afc7be
 );
 
 workbox.core.setCacheNameDetails({prefix: "antd-admin"});
