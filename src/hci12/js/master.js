57a67e430ef69147a408e7cd1b7621a7de3900c7
Nguyen Anh Phuong <nguyenanhphuong15101998@gmail.com>
2019-12-18 00:18:40 +0700

    add trang dang nhap

--- src/hci12/js/master.js
+++ src/hci12/js/master.js
@@ -1,5 +1,16 @@
 
-$( ".collapsed" ).click(function() {
-		$( ".item" ).css("background-color", "#161B21");
-		$( ".collapsed" ).css("background-color", "#161B21");
-});
+// $( ".collapsed" ).click(function() {
+// 		$( ".item" ).css("background-color", "#161B21");
+// 		$( ".collapsed" ).css("background-color", "#161B21");
+// });
+
+function myFunction(){
+            if(document.getElementById("name").value === "manager" && document.getElementById("password").value === "manager"){
+                window.location.replace('./leader/home.html');
+            }
+            else if(document.getElementById("name").value === "employee" && document.getElementById("password").value === "employee"){
+                window.location.replace('./employee/home.html');
+            }else{
+                 document.getElementById("demo").innerHTML = "Sai thông tin đăng nhập!";
+            }
+        }
